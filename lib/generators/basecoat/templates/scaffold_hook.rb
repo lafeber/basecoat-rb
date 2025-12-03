83ea462cc7db601412e3fb1130e9ac0e344a9726
Martijn Lafeber <martijn.lafeber@mobiliteitsfabriek.nl>
2025-12-03 22:07:13 +0100

    Use lucide-rails icons

--- lib/generators/basecoat/templates/scaffold_hook.rb
+++ lib/generators/basecoat/templates/scaffold_hook.rb
@@ -14,11 +14,7 @@ begin
       link_html = <<~HTML
             <li>
               <%= link_to #{plural_table_name}_path, data: { turbo_frame: "main_content", turbo_action: "advance" } do %>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                  <path d="m7 11 2-2-2-2" />
-                  <path d="M11 13h4" />
-                  <rect width="18" height="18" x="3" y="3" rx="2" ry="2" />
-                </svg>
+                <%= lucide_icon "square-terminal" %>
                 <span>#{human_name.pluralize}</span>
               <% end %>
             </li>
